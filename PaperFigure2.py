7cc45f006b715d7e06d9320f5bb863cc8fe54397
Afreveletti <asf20p@fsu.edu>
2026-02-27 17:24:53 -0600

    Update PaperFigure2.py
    Updated Figure 2 to include new pattern in CESM1.

--- PaperFigure2.py
+++ PaperFigure2.py
@@ -31,8 +31,8 @@ def create_LFP_EOF(lfps, indices, s, numLFPS):
 
 models = ['CESM1', 'CESM2', 'GFDL-CM3', 'E3SMv2']
 
-pacific_data = [np.load(path + f"FigureFiles/Pacific_{model}_ensemble_lfca_outputs.npz", allow_pickle=True) for model in models]
-Forced_Unforced_TS_data = [xr.open_dataset(path + f'processed_runs/filtered_data/{model}_Pacific_forced_unforced_TS_25_modes.nc') for model in models]
+pacific_data = [np.load(path + f"FigureFiles/Pacific_{model}_ensemble_lfca_outputs_new.npz", allow_pickle=True) for model in models]
+Forced_Unforced_TS_data = [xr.open_dataset(path + f'processed_runs/filtered_data/{model}_Pacific_forced_unforced_TS_25_modes_new.nc') for model in models]
 stopVals = [(int(Forced_Unforced_TS_data[i].mode.values) + 1) for i in range(len(models))]
 ref_lfps = [pacific_data[i]["ref_lfps"] for i in range(len(models))]
 indices = [pacific_data[i]['indices'] for i in range(len(models))]
@@ -42,9 +42,9 @@ ref_pvar_LFP = [pacific_data[i]['ref_pvar_LFP'] for i in range(len(models))]
 
 ref_LFPs_filled = [create_LFP_EOF(ref_lfps[i], indices[i], s[i], stopVals[i]) for i in range(len(models))]
 
-#set the LFCs and LFPs, fill models that don't have 4 reference modes with nan
+#set the LFCs and LFPs
 ref_LFPs_plot = np.full((4,3,46,90), np.nan)
-ref_LFPs_plot[0, :2] = ref_LFPs_filled[0][:2]
+ref_LFPs_plot[0] = ref_LFPs_filled[0][:3]
 ref_LFPs_plot[1] = ref_LFPs_filled[1][:3]
 ref_LFPs_plot[2] = ref_LFPs_filled[2][:3]
 ref_LFPs_plot[3] = ref_LFPs_filled[3][:3]
@@ -70,7 +70,7 @@ ref_lfcs = [pacific_data[i]["ref_lfcs"] for i in range(len(models))]
 
 # have to set the time from 1: for the back 3 models because CESM1 starts from 1920-02
 ref_lfcs_plot = np.full((4,1260,3), np.nan)
-ref_lfcs_plot[0, :, :2] = ref_lfcs[0][:,:2]
+ref_lfcs_plot[0, :, :3] = ref_lfcs[0][:,:3]
 ref_lfcs_plot[1] = ref_lfcs[1][1:,:3]
 ref_lfcs_plot[2] = ref_lfcs[2][1:,:3]
 ref_lfcs_plot[3] = ref_lfcs[3][1:,:3]
@@ -94,9 +94,10 @@ for i in range(3): # loop over modes
     ax_line.xaxis.set_major_locator(mdates.YearLocator(10))
     ax_line.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
     ax_line.set_xlim(time[0], time[-1])
-    ax_line.tick_params(axis="x", rotation=40)
+    ax_line.tick_params(axis="x", rotation=40, labelsize = 12)
     ax_line.set_ylim(-4, 4)
-    ax_line.set_ylabel("Standard Deviations", fontsize='large')
+    ax_line.tick_params(axis="y", labelsize = 12)
+    ax_line.set_ylabel("Standard Deviations", fontsize=13)
     ax_line.grid()
 
 #---------------------------------------------------------------------------------------------------------------------------------------
@@ -109,7 +110,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[0,0][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[0][0]:.2f}  {models[0]} LFP 1  {ref_pvar_LFP[0][0]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[0][0]:.2f} {models[0]} LFP 1 {ref_pvar_LFP[0][0]:.1f}%', fontsize= 'x-large')
 
 
 
@@ -121,7 +122,20 @@ pcm = ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[0,1][:, sorted_indices]
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[0][1]:.2f}  {models[0]} LFP 2  {ref_pvar_LFP[0][1]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[0][1]:.2f} {models[0]} LFP 2 {ref_pvar_LFP[0][1]:.1f}%', fontsize='x-large')
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+ax_map = fig.add_subplot(gs[2, 1], projection=pacific_proj)
+pcm = ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[0,2][:, sorted_indices]*-1,
+                  cmap='seismic', vmin=-0.5, vmax=0.5,
+                  transform=ccrs.PlateCarree())
+ax_map.coastlines()
+ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
+ax_map.set_aspect('auto')
+ax_map.set_title(f'{ref_r[0][2]:.2f} {models[0]} LFP 3 {ref_pvar_LFP[0][2]:.1f}%', fontsize='x-large')
+
 
 #---------------------------------------------------------------------------------------------------------------------------------------
 ############ CESM2
@@ -133,7 +147,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[1,0][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[1][0]:.2f}  {models[1]} LFP 1  {ref_pvar_LFP[1][0]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[1][0]:.2f} {models[1]} LFP 1 {ref_pvar_LFP[1][0]:.1f}%', fontsize='x-large')
 
 
 
@@ -145,7 +159,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[1,1][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[1][1]:.2f}  {models[1]} LFP 2  {ref_pvar_LFP[1][1]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[1][1]:.2f} {models[1]} LFP 2 {ref_pvar_LFP[1][1]:.1f}%', fontsize='x-large')
 
 
 
@@ -158,7 +172,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[1,2][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[1][2]:.2f}  {models[1]} LFP 3  {ref_pvar_LFP[1][2]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[1][2]:.2f} {models[1]} LFP 3 {ref_pvar_LFP[1][2]:.1f}%', fontsize='x-large')
 
 
 
@@ -172,7 +186,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[2,0][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[2][0]:.2f}  {models[2]} LFP 1  {ref_pvar_LFP[2][0]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[2][0]:.2f} {models[2]} LFP 1 {ref_pvar_LFP[2][0]:.1f}%', fontsize='x-large')
 
 
 
@@ -184,7 +198,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[2,1][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[2][1]:.2f}  {models[2]} LFP 2  {ref_pvar_LFP[2][1]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[2][1]:.2f} {models[2]} LFP 2 {ref_pvar_LFP[2][1]:.1f}%', fontsize='x-large')
 
 
 
@@ -197,7 +211,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[2,2][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[2][2]:.2f}  {models[2]} LFP 3  {ref_pvar_LFP[2][2]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[2][2]:.2f} {models[2]} LFP 3 {ref_pvar_LFP[2][2]:.1f}%', fontsize='x-large')
 
 
 
@@ -212,7 +226,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[3,0][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[3][0]:.2f}  {models[3]} LFP 1  {ref_pvar_LFP[3][0]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[3][0]:.2f} {models[3]} LFP 1 {ref_pvar_LFP[3][0]:.1f}%', fontsize='x-large')
 
 
 
@@ -224,7 +238,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[3,1][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[3][1]:.2f}  {models[3]} LFP 2  {ref_pvar_LFP[3][1]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[3][1]:.2f} {models[3]} LFP 2 {ref_pvar_LFP[3][1]:.1f}%', fontsize='x-large')
 
 
 
@@ -237,7 +251,7 @@ ax_map.pcolormesh(shifted_lon, lats, ref_LFPs_plot[3,2][:, sorted_indices],
 ax_map.coastlines()
 ax_map.set_extent([100, 300, -45, 70], crs=ccrs.PlateCarree())
 ax_map.set_aspect('auto')
-ax_map.set_title(f'{ref_r[3][2]:.2f}  {models[3]} LFP 3  {ref_pvar_LFP[3][2]:.2f}%', fontsize='large')
+ax_map.set_title(f'{ref_r[3][2]:.2f} {models[3]} LFP 3 {ref_pvar_LFP[3][2]:.1f}%', fontsize='x-large')
 
 
 # -------------------------
@@ -254,3 +268,5 @@ plt.subplots_adjust(hspace=0.55, top=0.90)
 cax = fig.add_axes([0.925, 0.225, 0.015, 0.55])
 cb = fig.colorbar(pcm, cax=cax, orientation='vertical')
 cb.set_label(r"$^\circ$C/STD", size=16)
+
+plt.savefig("Figures/Figure2.png", dpi=800)
